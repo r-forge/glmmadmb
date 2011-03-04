7f9750a7a782779573858bcc504c7866b065f219
bbolker <bbolker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-04 01:08:13 +0000

    added code to compare glmmADMB fit with (1) lme4 fits and (2) crossed random effects as custom TPL file
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/glmmadmb@59 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/multirand/tests/multrand.R
+++ branches/multirand/tests/multrand.R
@@ -5,27 +5,92 @@ set.seed(101)
 ## nrep <- 10
 
 ## smaller for quicker testing
-nblock <- 5
-nrep <- 50
-d <- expand.grid(f=factor(LETTERS[1:nblock]),
-                 g=factor(letters[1:nblock]),
-                 rep=1:nrep)
-N <- nrow(d)
-d$x <- runif(N)
-u_f <- rnorm(nblock,sd=1)
-u_g <- rnorm(nblock,sd=2)
-beta <- c(1,2)
-eta <- model.matrix(~x,data=d) %*% beta + u_f[as.numeric(d$f)]+u_g[as.numeric(d$g)]
-d$y <- rpois(N,exp(eta))
-
-g2 <- glmm.admb(y~x+(1|f)+(1|g),family="poisson",data=d)
+simfun <- function(seed=101,
+                   nblock=5,
+                   nrep=50,
+                   rsd=c(f=1,g=2),
+                   beta=c(1,2)) {
+  if (!is.null(seed)) set.seed(seed)
+  d <- expand.grid(f=factor(LETTERS[1:nblock]),
+                   g=factor(letters[1:nblock]),
+                   rep=1:nrep)
+  N <- nrow(d)
+  d$x <- runif(N)
+  u_f <- rnorm(nblock,sd=rsd["f"])
+  u_g <- rnorm(nblock,sd=rsd["g"])
+  eta <- model.matrix(~x,data=d) %*% beta + u_f[as.numeric(d$f)]+u_g[as.numeric(d$g)]
+  d$y <- rpois(N,exp(eta))
+  attr(d,"reff") <- rbind(data.frame(eff="f",block=levels(d$f),u=u_f),
+                          data.frame(eff="g",block=levels(d$g),u=u_g))
+  d
+}
+                          
+                               
+library(lme4)
+d1 <- simfun()
+m1 <- glmer(y~x+(1|f)+(1|g),family="poisson",data=d)
+res1 <- cbind(attr(d1,"reff"),glmer_est=c(unlist(ranef(m1))))
+g1 <- glmm.admb(y~x+(1|f)+(1|g),family="poisson",data=d1)
+res1 <- cbind(res1,glmer_est=c(unlist(ranef(m1))))
 
 summary(g2)
 coef(g2)
 VarCorr(g2)
+g2$U
 
-## 
+
+dd <- data.frame(expand.grid(x=1:5,eff=c("f","g"),
+                       method=c("glmmadmb","glmer") ),
+           re = c(unlist(g2$U),unlist(ranef(m2))))
+dd2 <- recast(dd,...~method,id.var=1:3)
+dd2$re <- c(matrix(c(u_f,u_g),nrow=2,byrow=TRUE))
+library(ggplot2)
+ggplot(dd2,aes(x=re,y=glmer))+geom_point()+facet_grid(.~eff)+
+  geom_abline(intercept=0,slope=1)+
+  geom_point(aes(y=glmmadmb),colour="red")
+##
+
+## random effects are PROPORTIONAL but not IDENTICAL:
+##  different scaling in each case?
+##  neither matches true values very well
 plot(u_f,g2$U$f,xlab="true",ylab="estimated")
 abline(a=0,b=1)
 plot(g2$U$g,u_g)
 abline(a=0,b=1)
+
+###############
+
+d2 <- simfun(nrep=200)
+m3 <- glmer(y~x+(1|f)+(1|g),family="poisson",data=d2)
+cbind(attr(d2,"reff"),est=c(unlist(ranef(m3))))
+
+g2 <- glmm.admb(y~x+(1|f)+(1|g),family="poisson",data=d)
+
+
+d3 <- simfun(nrep=500)
+m4 <- glmer(y~x+(1|f)+(1|g),family="poisson",data=d3)
+ranef(m4)
+
+if (require(R2admb)) {
+  ## it was easier for me to do this via R2admb ...
+  Zf <- model.matrix(~f-1,data=d2)
+  Zg <- model.matrix(~g-1,data=d2)
+  X <- model.matrix(~y,data=d2)
+  admb_dat <- list(Zf=Zf,Zg=Zg,X=X,y=d2$y,nobs=nrow(d2))
+                 
+  setup_admb()
+  g3 <- do_admb("crossed",data=admb_dat,
+                params=list(beta=c(1,0),
+                  sigma_f=1,sigma_g=1),
+                re=TRUE,
+                re_vectors=c(u_f=length(levels(d2$f)),
+                  u_g=length(levels(d2$g))),
+                ## checkparam="write",
+                ## checkdata="write",
+                ## extra.args="-l1 10000000 -l2 100000000 -l3 10000000 -nl1 10000000",
+                extra.args="-ndb 2 -l1 1000000",
+                verbose=TRUE)
+}
+
+
+
