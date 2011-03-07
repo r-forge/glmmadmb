8ff2ed56806d6e8c6b8b416c97ff1b6629430a2d
bbolker <bbolker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-07 15:24:45 +0000

    tests for a single random effect
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/glmmadmb@60 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/tests/singlerand.R
@@ -0,0 +1,47 @@
+## tests of a single random grouping variable
+library(glmmADMB)
+
+## random-intercept model
+set.seed(101)
+nblock <- 10
+nrep <- 10
+d <- expand.grid(f=factor(LETTERS[1:nblock]),rep=1:nrep)
+N <- nrow(d)
+d$x <- runif(N)
+u <- rnorm(nblock,sd=1)
+beta <- c(1,2)
+eta <- model.matrix(~x,data=d) %*% beta + u[as.numeric(d$f)]
+d$y <- rpois(N,exp(eta))
+g1 <- glmm.admb(y~x,random=~1,group="f",family="poisson",data=d)
+## identical 
+coef(g1)
+g1$stdbeta
+logLik(g1)
+g1$S
+g1$sd_S
+summary(g1$U)
+
+## random intercepts and slopes
+set.seed(101)
+nblock <- 10
+nrep <- 10
+d2 <- expand.grid(f=factor(LETTERS[1:nblock]),rep=1:nrep)
+N <- nrow(d)
+d2$x <- runif(N)
+u_f <- rnorm(nblock,sd=1)
+u_fx <- rnorm(nblock,sd=0.5)
+beta <- c(1,2)
+eta <- model.matrix(~x,data=d2) %*% beta + u[as.numeric(d2$f)]+
+  u[as.numeric(d2$f)]*d$x
+d2$y <- rpois(N,exp(eta))
+
+g2 <- glmm.admb(y~x,random=~x,group="f",family="poisson",data=d2)
+coef(g2)
+logLik(g2)
+g2$U
+summary(fitted(g2))
+g2$S
+g2$sd_S
+
+(fm <- glmm.admb(y~Base*trt+Age+Visit+(Visit|subject),
+                 data=epil2, family="nbinom",verbose=TRUE))
