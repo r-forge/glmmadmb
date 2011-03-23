37de4b6471f26c0d26bb204b4b31bfc10305ce51
bbolker <bbolker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-23 19:58:02 +0000

    update ranef() to scale by standard deviation   set up methods to play more nicely with lme4
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/glmmadmb@70 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/multirand/R/methods.R
+++ branches/multirand/R/methods.R
@@ -3,12 +3,28 @@ coef.glmm.admb <- function(object, ...) {
   object$b
 }
 
-ranef <- function(object, ...) {
-  UseMethod("ranef")
-}
+## need to make sure that this plays nicely with
+##  lme4 (S4 methods).  Not sure how.
+
+## ranef <- function(object, ...) {
+##   UseMethod("ranef")
+##}
+
+## setGeneric("ranef", function(object, ...) {
+##     standardGeneric("ranef")
+## })
+
+## setMethod("ranef","glmm.admb",
+##           function(object, sd=FALSE, ...) {
+##    if(sd) return(object$sd_U)
+##    mapply(sweep,object$U,lapply(object$S,function(z)sqrt(diag)),
+##           MoreArgs=list(MARGIN=2,FUN="*"),SIMPLIFY=FALSE)
+##  })
 
 ranef.glmm.admb <- function(object, sd=FALSE, ...) {
-  if(sd) object$sd_U else object$U
+  if(sd) return(object$sd_U)
+  mapply(sweep,object$U,lapply(object$S,function(z)sqrt(diag(z))),
+         MoreArgs=list(MARGIN=2,FUN="*"),SIMPLIFY=FALSE)
 }
 
 residuals.glmm.admb <- function(object, type=c("pearson", "response"), ...) {
@@ -40,9 +56,9 @@ nobs.glmm.admb <- function(object,...) {
   length(object$fitted)
 }
 
-VarCorr <- function(x,...) {
-  UseMethod("VarCorr")
-}
+## VarCorr <- function(x,...) {
+##  UseMethod("VarCorr")
+## }
 
 VarCorr.glmm.admb <- function(x,...) {
   x$S
