674fe7166791b0dcbeb0d6918b837f59dc13d5bc
bbolker <bbolker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-04-16 18:13:34 +0000

    added starfish (culcita) predation example   tweaked summary functions   added (now working) epil with nbinom
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/glmmadmb@75 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branchtests/singlerand_batch.R
+++ branchtests/singlerand_batch.R
@@ -1,5 +1,8 @@
 ## should be run with --vanilla
 
+## compare all known examples with a SINGLE random variable that
+## glmm.admb (and other models) are capable of.
+
 ## simulated data, intercept-only RE
 simfun <- function(seed=101,nblock=10,nrep=10,sd=c(int=1,slope=0),beta=c(1,2)) {
   if (!is.null(seed)) set.seed(seed)
@@ -17,6 +20,14 @@ simfun <- function(seed=101,nblock=10,nrep=10,sd=c(int=1,slope=0),beta=c(1,2)) {
 ## simulated data, slope+intercept RE
 d <- simfun()
 d2 <- simfun(sd=c(int=1,slope=0.5))
+
+cdata <- read.csv("culcitalogreg.csv",
+              colClasses=c(rep("factor",2),
+                "numeric",
+                rep("factor",6)))
+cdata$block <- factor(cdata$block,levels=1:10)
+
+cdata2 <- cdata[order(cdata$block),]
 
 ##
 data(epil2,package="glmmADMB")
@@ -44,6 +55,14 @@ t6_old <- system.time(g6_old <- glmm.admb(y~Base*trt+Age+Visit,
 t7_old <- system.time(g7_old <- glmm.admb(y~Base*trt+Age+Visit, 
                                           random=~Visit, group="subject",
                                           data=epil2, family="poisson"))
+
+t8_old <- system.time(g8_old <- glmm.admb(predation~ttt,
+                                          random=~1,
+                                          group="block",family="binomial",
+                                          link="logit",
+                                          data=cdata,
+                                          save.dir="g8oldtest"))
+
 save.image("singlerand_batch.RData")
 
 detach("package:glmmADMB.old")
@@ -68,6 +87,37 @@ t6_new <- system.time(g6_new <- try(glmm.admb(y~Base*trt+Age+Visit+
 t7_new <- system.time(g7_new <- glmm.admb(y~Base*trt+Age+Visit+ 
                                           (Visit|subject),
                                           data=epil2, family="poisson"))
+t8_new <- system.time(g8_new <- glmm.admb(predation~ttt+(1|block),
+                                          family="binomial",data=cdata,
+                                          save.dir="g8newtest"))
+t8_new2 <- system.time(g8_new2 <- glmm.admb(predation~ttt+(1|block),
+                                            family="binomial",data=cdata2,
+                                            save.dir="g8newtest2"))
+
+
+library("lme4")
+t0_lme4 <- system.time(g0_lme4 <- glmer(y~1+(1|f),
+                                          family="poisson",data=d))
+t1_lme4 <- system.time(g1_lme4 <- glmer(y~x+(1|f),
+                                          family="poisson",data=d))
+t2_lme4 <- system.time(g2_lme4 <- glmer(y~x+(1|f)+(0+x|f),
+                                          family="poisson",data=d))
+t3_lme4 <- system.time(g3_lme4 <- glmer(y~1+(1|f),
+                                          family="poisson",data=d2))
+t4_lme4 <- system.time(g4_lme4 <- glmer(y~x+(1|f),
+                                          family="poisson",data=d2))
+t5_lme4 <- system.time(g5_lme4 <- glmer(y~x+(1|f)+(0+x|f),
+                                          family="poisson",data=d2))
+## t6_lme4 <- system.time(g6_lme4 <- try(glmer(y~Base*trt+Age+Visit+
+##                                           (Visit|subject),
+##                                           data=epil2, family="nbinom")))
+## epil2 data fitted with Poisson, for comparison to lme4
+t7_lme4 <- system.time(g7_lme4 <- glmer(y~Base*trt+Age+Visit+ 
+                                          (1|subject)+(0+Visit|subject),
+                                          data=epil2, family="poisson"))
+
+t8_lme4 <- system.time(g8_lme4 <- glmer(predation~ttt+(1|block),family=binomial,data=cdata))
+
 save.image("singlerand_batch.RData")
 detach("package:glmmADMB")
 
@@ -91,6 +141,9 @@ t5_lme4 <- system.time(g5_lme4 <- glmer(y~x+(1|f)+(0+x|f),
 t7_lme4 <- system.time(g7_lme4 <- glmer(y~Base*trt+Age+Visit+ 
                                           (1|subject)+(0+Visit|subject),
                                           data=epil2, family="poisson"))
+
+t8_lme4 <- system.time(g8_lme4 <- glmer(predation~ttt+(1|block),family=binomial,data=cdata))
+
 save.image("singlerand_batch.RData")
 
 ## only does Poisson/binomial random-intercept models (2,5,6,7 not possible)
@@ -106,6 +159,12 @@ t3_glmmML <- system.time(g3_glmmML <- glmmML(y~1,cluster=f,
                                              control=list(maxit=1000)))
 t4_glmmML <- system.time(g4_glmmML <- glmmML(y~x,cluster=f,
                                           family="poisson",data=d2))
+
+t8_glmmML <- system.time(g8_glmmML  <- glmmML(predation~ttt,
+                                              family=binomial,
+                                              data=cdata,cluster=block))
+
+
 ## convergence problem on models 1, 3
 detach("package:glmmML")
 save.image("singlerand_batch.RData")
