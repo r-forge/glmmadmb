4c9ff0aed4c9415c1daba96f91f4ea8c00121285
bbolker (Ben  Bolker) <bolker@mcmaster.ca>
2011-07-06 18:25:22 +0000

    cleaned up tests, added output file
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/glmmadmb@123 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/multirand/tests/singlerand.R
+++ branches/multirand/tests/singlerand.R
@@ -13,23 +13,19 @@ beta <- c(1,2)
 eta <- model.matrix(~x,data=d) %*% beta + u[as.numeric(d$f)]
 d$y <- rpois(N,exp(eta))
 
-g1 <- glmmadmb(y~x+(1|f),family="poisson",data=d)
-coef(g1)
-g1$stdbeta
-logLik(g1)
-g1$S
-g1$sd_S
-summary(g1$U$f) ## not quite identical
+(g1 <- glmmadmb(y~x+(1|f),family="poisson",data=d))
+summary(g1)
 
 library(lme4)
 m1 <- glmer(y~x+(1|f),family="poisson",data=d)
-r1 <- ranef(g1)[[1]]
-r2 <- ranef(m1)[[1]][[1]]
-## ranef(mer) returns DATA FRAME so need additional unwrapping
+r1 <- unname(unlist(ranef(g1)))
+r2 <- unname(unlist(ranef(m1)))
+## ranef() returns DATA FRAME so need additional unwrapping
 plot(r1,r2,xlab="glmmADMB random effects",ylab="glmer random effects")
 abline(a=0,b=1)
-## proportional but 'shrunk' slightly differently
-summary(lm(r2~r1)) ## slope=0.9387
+
+stopifnot(all.equal(fixef(g1),fixef(m1),tolerance=1e-6))
+stopifnot(all.equal(r1,r2,tolerance=2e-4))
 
 ## random intercepts and slopes
 set.seed(101)
@@ -45,15 +41,16 @@ eta <- model.matrix(~x,data=d2) %*% beta + u[as.numeric(d2$f)]+
   u[as.numeric(d2$f)]*d2$x
 d2$y <- rpois(N,exp(eta))
 
-g2 <- glmmadmb(y~x+(x|f),family="poisson",data=d2)
-coef(g2)
-logLik(g2)
-g2$U$f ## not quite identical
-summary(fitted(g2))
-g2$S
-g2$sd_S
+(g2 <- glmmadmb(y~x+(x|f),family="poisson",data=d2))
 summary(g2)
+m2 <- glmer(y~x+(1|f)+(0+x|f),family="poisson",data=d2)
+r1 <- unname(unlist(ranef(g2)))
+r2 <- unname(unlist(ranef(m2)))
+
+stopifnot(all.equal(fixef(g2),fixef(m2),tolerance=1e-4))
+stopifnot(all.equal(r1,r2,tolerance=1.5e-3))
 
 epil2$subject <- factor(epil2$subject)
-fm <- glmmadmb(y~Base*trt+Age+Visit+(Visit|subject),
-                 data=epil2, family="nbinom")
+(fm <- glmmadmb(y~Base*trt+Age+Visit+(Visit|subject),
+                 data=epil2, family="nbinom"))
+summary(fm)
