5c33be6bb08e933ca7c5b715ac20fa4f5458989e
bbolker (Ben  Bolker) <bolker@mcmaster.ca>
2012-03-07 17:28:18 +0000

    anova slightly more flexible (rearranges in df order, checks for increasing LL)    process_randformula also returns name of random effects (for future checking)    use dat_write from R2admb
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/glmmadmb@190 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/tests/anova.R
@@ -0,0 +1,8 @@
+library(glmmADMB)
+data(bacteria,package="MASS")
+bacteria$present <- as.numeric(bacteria$y)-1
+bfit <-  glmmadmb(present ~ trt + I(week > 2), random = ~ 1 | ID,
+                  family = "binomial", data = bacteria)
+bfit2 <- update(bfit, .~.-trt)
+anova(bfit,bfit2)
+anova(bfit2,bfit)
