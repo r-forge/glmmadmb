5c42f8765c8786ae9bb443440673deabbc14dcd0
bbolker <bbolker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-03-30 13:56:51 +0000

    tweaks to printing   removed unused 'pkgname'
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/glmmadmb@208 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/print.glmm.admb.R
+++ pkg/R/print.glmm.admb.R
@@ -9,12 +9,12 @@ print.glmmadmb <- function(x, ...)
 
   cat("\nGLMM's in R powered by AD Model Builder:\n\n")
   cat("  Family:", object$family, "\n")
-  if(object$family == "nbinom")
-    cat("  alpha =", object$alpha, "\n")
-  if(!is.null(object$link) && object$family=="nbinom")
-    cat("  link =", object$link, "\n")
+  if(!is.null(object$family)) ## == "nbinom")
+      cat("  alpha =", object$alpha, "\n")
+  if(!is.null(object$link)) ##  && object$family=="nbinom")
+      cat("  link =", object$link, "\n")
   if(object$zeroInflation)
-    cat("  Zero inflation: p =", object$pz, "\n")
+      cat("  Zero inflation: p =", object$pz, "\n")
   cat("\nFixed effects:\n")
   cat("  Log-likelihood:", object$loglik, "\n")
   cat("  AIC:", AIC(object), "\n")
@@ -45,7 +45,7 @@ print.glmmadmb <- function(x, ...)
     if(sd_S_print)
     {
       cat("\nCovariance matrix of random effects vector (left) and corresponding standard deviations (right): \n\n")
-      print(cbind(object$S,NA,NA,NA,NA,object$sd_S), na.print="")
+      print(cbind(object$S,NA,NA,NA,NA,object$sd_S), na.print="")  ## FIXME: does this work for a non-4x4 matrix??
       cat("\nNote: The diagonal elements of the above left matrix are variances, NOT std's.\n")
     }
   }
