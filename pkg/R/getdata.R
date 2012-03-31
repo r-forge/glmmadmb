3574cb4ade1b747838a423493c98f99d6186d44e
bbolker (Ben  Bolker) <bolker@mcmaster.ca>
2012-03-31 15:25:55 +0000

    fix getdata() (load into parent frame)   corresponding tweaks
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/glmmadmb@210 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/getdata.R
+++ pkg/R/getdata.R
@@ -2,6 +2,8 @@ getdata <- function(s) {
     ## FIXME: if s is missing, generate list of available files
     ## FIXME: be more flexible about extensions
     ## FIXME: can data() be adapted for this??
+    p <- parent.frame()
     load(system.file("extdata",paste(s,".rda",sep=""),
-                    package="glmmADMB"))
+                    package="glmmADMB"),envir=p)
 }
+
