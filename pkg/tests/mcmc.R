c1987937c107a76f45c291b88cd139e96c76f774
bbolker <bbolker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-07-02 16:09:08 +0000

    add stub help file for step   update test output   comment out not-working mcmc test
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/glmmadmb@214 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/mcmc.R
+++ pkg/tests/mcmc.R
@@ -28,19 +28,28 @@ fixef(g1M)
 
 ### try example from simon.chamaille@cefe.cnrs.fr
 
-nblock <- 10
+nblock <- 8
 indperblock <- 10
-nperind <- 10
+nperind <- 5
 sd.u <- 1
-ntot <- nblock*indperblock*
-d <- data.frame(x=runif(ntot),f=factor(rep(LETTERS[1:nblock],each=nperblock)))
-r <- rnorm(nblock,mean=0,sd=sd.u)
-d$eta <- with(d,0.2+0.5*x+r[f])
+ntot <- nblock*indperblock*nperind
+d <- expand.grid(f=factor(rep(LETTERS[1:nblock],each=nperblock)),
+                 ind=factor(rep(1:indperblock)),
+                 rep=rep(1:nperind))
+d$x <- runif(ntot)
+u1 <- rnorm(nblock,mean=0,sd=sd.u)
+u2 <- rnorm(nblock*indperblock,mean=0,sd=sd.u)
+d$eta <- with(d,0.2+0.5*x+u1[f]+u2[interaction(f,ind)])
 d$mu <- exp(d$eta)
 d$y <- rpois(ntot,lambda=d$mu)
 
-mod.admb <- glmmadmb(formula=response~1+(1|group/ind),
-                      data=somedata,
-                      family="nbinom1",link="log",
-                      admb.opts=admbControl(shess=FALSE,noinit=FALSE),
-                      mcmc=TRUE)
+if (FALSE) {
+    ## slow!
+    mod.admb <- glmmadmb(formula=y~1+(1|f/ind),
+                         data=d,
+                         family="nbinom1",link="log",
+                         admb.opts=admbControl(shess=FALSE,noinit=FALSE),
+                         mcmc=TRUE,mcmc.opts=mcmcControl(mcmc=100),
+                         extra.args="-ndi 60000",
+                         verbose=TRUE)
+}
